f1acb4c4a2549cb99767e68d079d2978085a2871
ingporto <ing.portocarrero@gmail.com>
2023-07-31 19:05:59 -0500

    tipos

---
+++ tipos/main.go
@@ -0,0 +1,43 @@
+package main
+
+func main() {
+	// NUMERO ENTEROS
+
+	// int depende del OS (32 o 64 bits)
+	// int8 = 8 bits = -128 a 127
+	// int16 = 16 bits = -32768 a 32767 = -2^15 a 2^15-1
+	// int32 = 32 bits = -2^31 a 2^31-1
+	// int64 = 64 bits = -2^63 a 2^63-1
+
+
+	// NÚMEROS ENTEROS POSITIVOS
+
+	// uint depende del OS (32 o 64 bits)
+	// uint8 = 8 bits = 0 a 255 = 0 a 2^8-1
+	// uint16 = 16 bits = 0 a 65535 = 0 a 2^16-1
+	// uint32 = 32 bits = 0 a 4294967295 = 0 a 2^32-1
+	// uint64 = 64 bits = 0 a 18446744073709551615 = 0 a 2^64-1
+
+
+	// NÚMERO DECIMALES
+
+	// float32 = 32 bits = +/-1.18e-38 a +/-3.4e+38
+	// float64 = 64 bits = +/-2.23e-308 a +/-1.8e+308
+
+
+	// TEXTOS
+
+	// string = " "
+
+
+	// BOOLEANOS
+
+	// bool = true o false
+
+
+	// NÚMEROS COMPLEJOS
+
+	// Complex64 = Real e imaginario float32
+	// Complex128 = Real e imaginario float64
+	// Ejemplo: c:= 1 + 2i
+}
